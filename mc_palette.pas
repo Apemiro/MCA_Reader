e69b66a56b24772a984e45e6cc5baf3a1f00f2b5
Apemiro <1157775623@qq.com>
2025-07-16 18:33:10 +0800

    temp2

--- mc_palette.pas
+++ mc_palette.pas
@@ -1,6 +1,7 @@
 unit mc_palette;
 
 {$mode objfpc}{$H+}
+{$ModeSwitch advancedrecords}
 
 interface
 
@@ -24,6 +25,9 @@ type
         data     : byte;
         rejected : boolean;
         color    : TColor;
+    public
+        function GetValue_Raw(BlockRecord:TMC_PaletteRecord):TMC_Block;
+        function GetValue_Colorize(BlockRecord:TMC_PaletteRecord):TMC_Block;
     end;
     PMC_PaletteRecord = ^TMC_PaletteRecord;
 
@@ -36,10 +40,23 @@ type
         procedure AddBlock(BlockName:string; BlockId:integer=-1; BlockData:byte=255; BlockColor:TColor=clBlack);
         procedure ClearSectionPatette;
         procedure AppendSectionPatette(BlockName:string);
-        function SectionIdToUniverseId(SectionId:integer):TMC_Block;
+        function FindBlockBySectionId(SectionId:integer):PMC_PaletteRecord;
+    public
+        constructor Create;
+        destructor Destroy; override;
     end;
 
 implementation
+
+function TMC_PaletteRecord.GetValue_Raw(BlockRecord:TMC_PaletteRecord):TMC_Block;
+begin
+    result:=TMC_Block((DWord(BlockRecord.id) shl 16) or (BlockRecord.data shl 8));
+end;
+
+function TMC_PaletteRecord.GetValue_Colorize(BlockRecord:TMC_PaletteRecord):TMC_Block;
+begin
+    result:=TMC_Block(BlockRecord.color);
+end;
 
 function TMC_Palette.FindBlock(BlockName:string):PMC_PaletteRecord;
 begin
@@ -53,7 +70,7 @@ end;
 
 procedure TMC_Palette.ClearSectionPatette;
 begin
-
+    SetLength(FSection,0);
 end;
 
 procedure TMC_Palette.AppendSectionPatette(BlockName:string);
@@ -61,13 +78,22 @@ begin
 
 end;
 
-function TMC_Palette.SectionIdToUniverseId(SectionId:integer):TMC_Block;
+function TMC_Palette.FindBlockBySectionId(SectionId:integer):PMC_PaletteRecord;
 begin
 
 end;
 
+constructor TMC_Palette.Create;
+begin
+    FUniverse := TStringList.Create;
+    SetLength(FSection,0);
+end;
 
-
+destructor TMC_Palette.Destroy;
+begin
+    FUniverse.Free;
+    SetLength(FSection,0);
+end;
 
 end.
 
